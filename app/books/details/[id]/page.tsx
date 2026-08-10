b13a0a80601a56c82c80c52759d8551d58c0bea6
Volodymyr Pashchenko <pikimelposhta@gmail.com>
2026-08-10 18:45:24 +0300

    update

--- app/books/details/[id]/page.tsx
+++ app/books/details/[id]/page.tsx
@@ -4,9 +4,11 @@ import css from "./page.module.css";
 interface PageProps {
   params: Promise<{ id: string }>;
 }
+
 const Page = async ({ params }: PageProps) => {
   const { id } = await params;
   const book = await getBookById(id);
+
   return (
     <div className={css["page"]}>
       <h1>{book.title}</h1>
